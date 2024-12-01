6949bbd372b59479b1b7487fefff5a171968b6c9
Sarthak Anil Janrao <119600876+Sarthakjanrao07@users.noreply.github.com>
2024-12-01 20:38:37 +0530

    vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  root: '.',
-=======
   root:'.',
->>>>>>> 4db2e40b1299de4d32d8e68c40e37db48d547534
   plugins: [react()],
   build: {
     outDir: 'dist', // Output directory for the build
+    rollupOptions: {
+      input: './index.html', // Entry file
+    },
   },
 })
